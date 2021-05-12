eb43cfb35bbca3e6f403910d5c72393fcba5e59d
Calpico <calpicodrink@protonmail.com>
2021-05-12 12:23:49 -0700

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Text } from 'react-native'
-import styled from 'styled-components/native'
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
 
 const Holder = styled.View`
-    flex: 1;
-    background: #fff;
-    align-items: center;
-    justify-content: center;
-`
+  flex: 1;
+  background: #fff;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
-    return (
-        <Holder>
-            <Text>Open up App.tsx to start working on your app!</Text>
-        </Holder>
-    )
+  return (
+    <Holder>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </Holder>
+  );
 }
