05eb6cb76c1ae6c71d9893b11daae91c793ac140
Calpico <calpicodrink@protonmail.com>
2021-06-08 09:19:51 -0700

    @root

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = function (api) {
                     ],
                     root: ['./src'],
                     alias: {
+                        '@root': './src',
                         '@util': './src/util',
                         '@pages': './src/pages',
                         '@styles': './src/styles',
